8f38a2d5261cfa7ed8b79df6d681cde44be3e45f
Thibault_Merelle <merellethibault@gmail.com>
2021-03-15 12:02:52 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from reverse import neg
+from reverse_str import rev_str
+from rev_digits import rev_digi
+from words_counter import w_count
+from average_lenth import av_str
+from palindrome import palind
+from double_check import doubles
+
+
+
+
+print(neg(46))
+
+print(rev_str('bonjour'))
+
+print(rev_digi(345))
+
+print(w_count("ici c'est simplon !!"))
+
+print(av_str("ici , c'est simplon !!"))
+
+print(palind('ada'))
+
+print(doubles([1, 2, 8, 8, 7, 1]))
+
